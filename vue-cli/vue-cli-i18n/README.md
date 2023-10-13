41492f8d62535e2cd11c379ab1f6214403cd94ff
jotape-exe <joaoxstone@gmail.com>
2023-10-13 17:55:56 -0300

    i18n cue-cli

---
+++ vue-cli/vue-cli-i18n/README.md
@@ -0,0 +1,31 @@
+# vue-cli-i18n
+
+https://github.com/intlify/vue-cli-plugin-i18n
+
+## Project setup
+```
+npm install
+```
+
+### i18n Dependencie
+```
+vue add i18n
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue-cli/vue-cli-i18n/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+import { ref } from 'vue'
+  import i18n from "@/i18n"
+
+  const selected = ref('')
+
+  const languages = ref([
+  { language: 'en-US', title: 'English' },
+  { language: 'pt-BR', title: 'Portugês BR' } 
+   
+  ])
+
+  const changeLocale = (locale) => {
+    i18n.global.locale = locale
+  }
+
+</script>
+
+<template>
+  <h1>i18n - Config</h1>
+
+  <select v-model="selected" @change="changeLocale(selected)">
+  <option disabled value="">{{  $t('selectLanguage') }}</option>
+  <option v-for="lang in languages" :key="lang.language" :value="lang.language">{{ lang.title  }}</option>
+</select>
+
+  <br>
+  <br>
+  Mensagem: {{ $t('welcome') }}
+  <br>
+  Idioma: {{ $t('language') }}
+
+  <br>
+  <h2> {{  $t('selectLanguage') }} </h2>
+
+  <h1>Data</h1>
+
+  <p>{{ $d (new Date(), 'short') }}</p>
+
+</template>
+
