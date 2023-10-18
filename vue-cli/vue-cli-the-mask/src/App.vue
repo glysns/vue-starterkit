717bac1a5708fa58a73cb5be039779b5e75200ad
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-10-18 11:58:01 -0300

    ajustes mask

--- vue-cli/vue-cli-the-mask/src/App.vue
+++ vue-cli/vue-cli-the-mask/src/App.vue
@@ -1,17 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <br>
+    <h1>Auxiliar</h1>
+    <button @click="preencher()">Preencher</button>
+    <button @click="limpar()">Limpar</button>
+    <br>
+    <label>
+      CPF:
+      <input type="text" v-model="cpf" v-mask="'###.###.###-##'"  />
+      CPF MASK:{{ cpf }}
+      CPF RAW: {{ unmask(cpf) }}
+    </label>
+    <br>
+    <label>
+      CNPJ:
+      <input type="text" v-model="cnpj" v-mask="'##.###.###/####-##'"  />
+      CNPJ MASK:{{ cnpj }}
+    </label>
+
+    <br>
+    <label>
+      CPF/CNPJ:
+      <input type="text" v-model="cpfCnpj" v-mask="['###.###.###-##', '##.###.###/####-##']"  />
+      CPF/CNPJ MASK:{{ cpfCnpj }}
+    </label>
+
+    <br>
+    <label>
+      TELEFONE:
+      <input type="text" v-model="telefone" v-mask="'(##) ####-####'"  />
+      TELEFONE:{{ telefone }}
+    </label>
+
+    <br>
+    <label>
+      CELULAR:
+      <input type="text" v-model="celular" v-mask="'(##) #####-####'"  />
+      CELULAR:{{ celular }}
+    </label>
+
+    <br>
+    <h1>Exibindo os campos sem mascara</h1>
+    <button @click="exibirDadosSemMascara()">Atualizar</button>
+    <p>{{ resultado }}</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+//:mask="user.language === 'ptBR' ? '(##) ####-####' : '(###) ###-####'"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    //variaveis da intancia do objeto que representam dados mediante a interação do usuário
+    return {
+      cpf:'',
+      cnpj:'',
+      cpfCnpj:'',
+      telefone:'',
+      celular:'',
+      telefoneCeluar:'',
+      decimal:'',
+      moeda:'',
+      resultado:''
+    };
+  },
+  methods: {
+      //este método poderia ser um mixin no vue
+      unmask(text) {
+        const defaultDelimiters = /[-!$%^&*()_+|~=`{}[\]:";'<>?,./\\ ]/
+        return text ? String(text).replace(new RegExp(defaultDelimiters, 'g'), '') : text
+      },
+      preencher(){
+        this.cpf='34829826002';
+        this.cnpj='98262897000126';
+        this.telefone='1133870967';
+        this.celular='11965450789';
+        this.decimal=10527.6543;
+        this.moeda=10527.6543;
+      },
+      limpar(){
+        this.cpf=null;
+        this.cnpj=null;
+        this.telefone=null;
+        this.celular=null;
+        this.decimal=null;
+        this.moeda=null;
+      },
+      exibirDadosSemMascara(){
+        this.resultado = 'resultado....';
+        const cpfRaw = this.unmask(this.cpf);
+        this.resultado = `CPF: ${cpfRaw}, CNPJ ${this.unmask(this.cnpj)}, TELEFONE ${this.unmask(this.telefone)}, CELULAR ${this.unmask(this.celular)}`;
+      }
+    },
+};
 </script>
 
 <style>
@@ -24,3 +105,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
--- vue-cli/vue-cli-the-mask/src/main.js
+++ vue-cli/vue-cli-the-mask/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+//lib para disponibilizar as diretivas e recursos
+import {mask} from 'vue-the-mask'
+
+const app = createApp(App)
+
+// apelidando a diretiva para nosso projeto
+app.directive('mask',mask)
+
+app.mount('#app')
