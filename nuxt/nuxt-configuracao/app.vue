e9388af44cc2e14786e8b3a7913d318b48d9ef29
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-10-24 16:55:56 -0300

    config

--- nuxt/nuxt-configuracao/app.vue
+++ nuxt/nuxt-configuracao/app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p>Ola</p>
   </div>
 </template>
--- nuxt/nuxt-configuracao/nuxt.config.ts
+++ nuxt/nuxt-configuracao/nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  alias:{
+    
+  },
+  app:{
+    //baseURL:process.env.BASE_URL || "http://localhost:3000"
+  },
+  css: [
+    // Load a Node.js module directly (here it's a Sass file).
+    //'bulma',
+    // CSS file in the project
+    '@/assets/css/main.css',
+    // SCSS file in the project
+    // '@/assets/css/main.scss'
+  ]
 })
