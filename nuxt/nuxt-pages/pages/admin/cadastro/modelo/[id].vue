d1b4608812043b86fbae3a43c75599c11e278bdd
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-10-26 17:12:53 -0300

    pages

---
+++ nuxt/nuxt-pages/pages/admin/cadastro/modelo/[id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Você está visualizando o modelo {{ $route.params.id }}
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ nuxt/nuxt-pages/pages/admin/cadastro/modelo/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Consulta de Modelos
+        <br>
+        Aqui aparecerá a tabela contendo as modelos
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
