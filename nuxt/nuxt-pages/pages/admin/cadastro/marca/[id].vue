1ed4f59c35abea247243b55a1653960080f9b9a0
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-10-26 17:31:02 -0300

    pages

--- nuxt/nuxt-pages/pages/admin/cadastro/marca/[id].vue
+++ nuxt/nuxt-pages/pages/admin/cadastro/marca/[id].vue
@@ -1,13 +1,12 @@
 <template>
     <div>
         Você está visualizando a marca {{ $route.params.id }}
+
+        Você está visualizando a marca pelo ID no Params {{ id }}
     </div>
 </template>
 
 <script setup>
-
+    const route = useRoute();
+    const id = computed(() => route.params.id);
 </script>
-
-<style lang="scss" scoped>
-
-</style>
