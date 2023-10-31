8416d96593c646e998288e067ea3e427f1124829
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-10-31 13:29:24 -0300

    props

--- nuxt/nuxt-server-api/pages/products/[id].vue
+++ nuxt/nuxt-server-api/pages/products/[id].vue
@@ -7,12 +7,17 @@
         <br><br>
         Descrição: {{ product.description }}
 
+        <br>
+        Imagem
+        <br>
+        <ProductImg :product="product"/>
+
     </div>
 </template>
 
 <script setup>
 
     const { id } = useRoute().params;
-    const {data : product} = await useFetch(`https://fakestoreapi.com/products/${id}`)
+    const {data : product} = await useFetch(`https://fakestoreapi.com/products/${id}`, {key:id})
   
 </script>
