40b8b5e59f1227987fad1b8d6cffe8483e4107b9
Gleyson Sampaio <80013043+glysns@users.noreply.github.com>
2023-10-31 11:33:40 -0300

    testando consumo de apis

---
+++ nuxt/nuxt-server-api/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      
+      <div v-for="i in products">
+        <NuxtLink :to="`/products/${i.id}`" style="color:blue"> {{ i.id }} -- {{ i.title }}</NuxtLink>
+        {{ i.description }}
+  
+      </div>
+  
+      <br> <br> <br> 
+      {{ cep }}
+    </div>
+  </template>
+  
+  <script setup>
+    const {data : products} = await useFetch('https://fakestoreapi.com/products')
+  
+    const {data : cep} = await useFetch('https://viacep.com.br/ws/01001000/json')
+
+    //const res = await useFetch('https://viacep.com.br/ws/0100100/json')
+    //console.log('resposta via cep', res)
+  
+  </script>
---
+++ nuxt/nuxt-server-api/pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+       Eu sou o conteúdo que será apresentado quando selecionarmos um produto
+        ID: {{ id }}
+        <br><br>
+        Titulo: {{ product.title }}
+        <br><br>
+        Descrição: {{ product.description }}
+
+    </div>
+</template>
+
+<script setup>
+
+    const { id } = useRoute().params;
+    const {data : product} = await useFetch(`https://fakestoreapi.com/products/${id}`)
+  
+</script>
